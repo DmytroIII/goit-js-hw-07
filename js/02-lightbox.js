522a25cb43a9d438213657e8947ccba1a2111c3e
Filatov Dmytro <dmitrovfilatov@gmail.com>
2022-10-01 00:54:33 +0300

    jk

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,5 +1,25 @@
-
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const paletteContainer = document.querySelector(".gallery");
+const galleryImages = createSliderImage(galleryItems);
+paletteContainer.innerHTML = galleryImages;
+
+function createSliderImage(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) => `
+    <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`
+    )
+    .join("");
+}
+
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
